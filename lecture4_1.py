1c037ce385e22488945ad4c110974e7acb9c3f03
dkinoshita <daisuke.kinoshita@atl-info.co.jp>
2019-01-01 00:29:37 +0900

    modify.

--- lecture4_1.py
+++ lecture4_1.py
@@ -1,13 +1,22 @@
 import sys
 args = sys.argv
 
-def main(a, b):
-    print(compareValue(a, b))
+class lecture4_1:
+    def __init__(self):
+        super().__init__()
 
-def compareValue(a, b):
-    if a >= b:
-        return a
-    elif b > a:
-        return b
+    def main(self, a, b):
+        print(self.compareValue(a, b))
+        otherClass = lecture4_1_2()
+        print(otherClass.compareValue2(a, b))
 
-main(args[1], args[2])
+    def compareValue(self, a, b):
+        if a >= b:
+            return a
+        elif b > a:
+            return b
+
+tmpArgs1 = args[1]
+tmpArgs2 = args[2]
+lecture4_1Class = lecture4_1()
+lecture4_1Class.main(tmpArgs1, tmpArgs2)
---
+++ lecture4_1_2.py
@@ -0,0 +1,8 @@
+import sys
+
+class lecture4_1_2:
+	def compareValue2(self, a, b):
+		if a >= b:
+			return a
+		elif b > a:
+			return b
